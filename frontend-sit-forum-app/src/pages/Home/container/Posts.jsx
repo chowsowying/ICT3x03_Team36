2d23337c0c651b95a7ff2bbe49b912215aed5b38
2102673 <90232284+2102673@users.noreply.github.com>
2023-10-31 22:09:36 +0800

    added hyperlink to "More articles" button

--- frontend-sit-forum-app/src/pages/Home/container/Posts.jsx
+++ frontend-sit-forum-app/src/pages/Home/container/Posts.jsx
@@ -4,10 +4,12 @@ import { FaArrowRight } from "react-icons/fa";
 import { isError, useQuery } from '@tanstack/react-query';
 import { getAllPost } from '../../../services/index/posts';
 import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 
 
 const Posts = () => {
+  const navigate = useNavigate();
 
   const { data, isLoading, isError } = useQuery({
     queryFn: () => getAllPost(),
@@ -18,6 +20,9 @@ const Posts = () => {
     }
   });
 
+  const handlePosts = () => {
+    navigate(`/posts`);
+  };
 
   return (
     <section className="container mx-auto px-5 py-10">
@@ -31,7 +36,7 @@ const Posts = () => {
       </div>
 
       <div className="mt-5">
-        <button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
+        <button onClick={handlePosts} className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
           <span>More articles</span>
           <FaArrowRight className="w-3 h-3" />
         </button>
